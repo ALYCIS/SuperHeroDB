20b107eaed33c86374d4cb4206431fe3fde63479
CISSE <alyhabcis@gmail.com>
2022-08-16 15:20:13 +0200

    Injection de l'interface avec @Inject ISuperHero superheros Ajout dans les imports  le @using SuperHeroDB.Client.Services Ajout de la dependacy injection dans le Program.cs

---
+++ SuperHeroDB/Client/Services/ISuperHeroService.cs
@@ -0,0 +1,13 @@
+﻿using SuperHeroDB.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB.Client.Services
+{
+    interface ISuperHeroService
+    {
+        Task<List<SuperHero>> GetSuperHeros();
+    }
+}
---
+++ SuperHeroDB/Client/Services/SuperHeroService.cs
@@ -0,0 +1,24 @@
+﻿using SuperHeroDB.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB.Client.Services
+{
+    public class SuperHeroService : ISuperHeroService
+    {
+        private readonly HttpClient _httpClient;
+
+        public SuperHeroService(HttpClient httpClient)
+        {
+            this._httpClient = httpClient;
+        }
+        public async Task<List<SuperHero>> GetSuperHeros()
+        {
+            return await _httpClient.GetFromJsonAsync<List<SuperHero>>("api/superhero");
+        }
+    }
+}
