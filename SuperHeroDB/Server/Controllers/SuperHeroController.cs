65ff2ba1591f907b136e4f2caeb15417f61aa977
CISSE <alyhabcis@gmail.com>
2022-08-16 13:48:58 +0200

    Ajout des classe SuperHero et Comic dans Shared. Ajout du controlleur vide.

---
+++ SuperHeroDB/Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroDB.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+          
+    }
+}
