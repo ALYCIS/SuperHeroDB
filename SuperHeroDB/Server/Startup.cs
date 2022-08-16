bbfd5c693f7874e7f201054139421afa1d6202ce
CISSE <alyhabcis@gmail.com>
2022-08-16 13:29:21 +0200

    Premier commit sans modification

--- SuperHeroDB/Server/Startup.cs
+++ SuperHeroDB/Server/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
-
+ 
 namespace SuperHeroDB.Server
 {
     public class Startup
