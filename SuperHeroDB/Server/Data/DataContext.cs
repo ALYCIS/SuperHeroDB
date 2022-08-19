f80a8f8b06fdb62355e08d8fc89835a9ca4ceb94
CISSE <alyhabcis@gmail.com>
2022-08-19 15:39:05 +0200

    Création du repertoire Data, pour mettre la classe DataContext.cs . Modification du fichier appsettings.json pour ajouter la : ConnectionStrings, en faisant la commande suivante: "ConnectionStrings":{     "DefaultConnection": "Server=localhost\\SQLEXPRESS; Database=superhero-db; Trusted_Connection=true;" }

---
+++ SuperHeroDB/Server/Data/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroDB.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB.Server.Data
+{
+    public class DataContext:DbContext
+    {
+        public DbSet<SuperHero> DbHeroes { get; set; }
+        public DbSet<Comic> DbComics { get; set; }
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Comic>().HasData(
+                new Comic { Id=1, Name="Marvel"},
+                new Comic { Id=2, Name="DC"}
+                );
+
+            modelBuilder.Entity<Comic>().HasData(
+                new SuperHero { Id = 1, FirstName = "Peter", LastName = "Parker", HeroName = "Spiderman", Comic = new Comic { Id = 1, Name = "Marvel" } },
+                new SuperHero { Id = 2, FirstName = "Bruce", LastName = "Wayne", HeroName = "Batman", Comic = new Comic { Id = 2, Name = "DC" } }
+                );
+        }
+    }
+}
