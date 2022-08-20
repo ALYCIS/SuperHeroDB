fd44c6402d39a4e6783dc8379c4e0387e364c98f
CISSE <alyhabcis@gmail.com>
2022-08-20 12:22:54 +0200

    Modification du nom des attributs pour respecter la convention.

--- SuperHeroDB/Server/Data/DataContext.cs
+++ SuperHeroDB/Server/Data/DataContext.cs
@@ -9,14 +9,14 @@ namespace SuperHeroDB.Server.Data
 {
     public class DataContext:DbContext
     {
-        public DbSet<SuperHero> DbHeroes { get; set; }
-        public DbSet<Comic> DbComics { get; set; }
+        public DbSet<SuperHero> SuperHero { get; set; }
+        public DbSet<Comic> Comics { get; set; }
         public DataContext(DbContextOptions<DataContext> options):base(options)
         {
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            /*base.OnModelCreating(modelBuilder);*/
             modelBuilder.Entity<Comic>().HasData(
                 new Comic { Id=1, Name="Marvel"},
                 new Comic { Id=2, Name="DC"}
